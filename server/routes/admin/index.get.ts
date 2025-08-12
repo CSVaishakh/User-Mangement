07317592d5b3af496ac6b684f9dc8d029659d6ac
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-08-12 23:07:22 +0530

    feat: update user role queries and improve error handling

--- server/routes/admin/index.get.ts
+++ server/routes/admin/index.get.ts
@@ -1,6 +1,6 @@
 import { createError, defineEventHandler, getQuery } from "h3";
 import axios from "axios"
-import { supabase } from "server/utils/supabase";
+import { supabase } from "../../utils/supabase"
 
 export default defineEventHandler ( async (event) => {
     const { token } = getQuery(event)
@@ -12,9 +12,16 @@ export default defineEventHandler ( async (event) => {
         })
     }
 
-    const { data : userid } = await axios.post("https://localhost:3000/verify",{token})
+    const { data : userid } = await axios.get("http://127.0.0.1:5000/verify", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    
+    console.log(token)
 
     if (!userid) {
+        console.log("invalid 1")
         throw createError({
             statusCode: 403,
             statusMessage: "Invalid Token"
@@ -24,9 +31,10 @@ export default defineEventHandler ( async (event) => {
     const { data: associates, error: associatesError } = await supabase
         .from("users")
         .select("*")
-        .eq("role", "associate");
+        .eq("role", "Associate");
 
     if (associatesError) {
+        console.log("invalid 2")
         throw createError({
             statusCode : 500,
             statusMessage : associatesError.message
@@ -37,14 +45,28 @@ export default defineEventHandler ( async (event) => {
     const { data: managers, error: managersError } = await supabase
         .from("users")
         .select("*")
-        .eq("role","manager")
+        .eq("role","Manager")
 
     if (managersError) {
+        console.log("invalid 3")
         throw createError({
             statusCode : 500,
             statusMessage : managersError.message
         })
     }
 
-    return { req_data: [associates,managers]}
+    const { data: users, error: usersError } = await supabase
+        .from("users")
+        .select("*")
+        .is("role",null)
+
+    if (usersError) {
+        console.log("invalid 4")
+        throw createError({
+            statusCode : 500,
+            statusMessage : usersError.message
+        })
+    }
+
+    return { req_data: [associates,managers,users]}
 })
