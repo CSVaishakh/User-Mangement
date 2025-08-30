69b2b6b7c3c0a553f382ad426361afe94f1ebab0
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-08-30 09:34:47 +0530

    fix : removed logging of token

--- server/routes/admin/index.get.ts
+++ server/routes/admin/index.get.ts
@@ -1,81 +1,78 @@
 import { createError, defineEventHandler, getQuery } from "h3";
-import axios from "axios"
-import { supabase } from "../../utils/supabase"
+import axios from "axios";
+import { supabase } from "../../utils/supabase";
 
-export default defineEventHandler ( async (event) => {
-    const { token } = getQuery(event)
+export default defineEventHandler(async (event) => {
+  const { token } = getQuery(event);
 
-    if (!token) {
-        throw createError({
-            statusCode: 401,
-            statusMessage : "Token not found"
-        })
-    }
+  if (!token) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Token not found",
+    });
+  }
 
-    const { data : userid } = await axios.get("http://127.0.0.1:5000/verify", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-    
-    console.log(token)
+  const { data: userid } = await axios.get("http://127.0.0.1:5000/verify", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    if (!userid) {
-        throw createError({
-            statusCode: 403,
-            statusMessage: "Invalid Token"
-        })
-    }
+  if (!userid) {
+    throw createError({
+      statusCode: 403,
+      statusMessage: "Invalid Token",
+    });
+  }
 
-    const { data: associates, error: associatesError } = await supabase
-        .from("users")
-        .select("*")
-        .eq("role", "Associate");
+  const { data: associates, error: associatesError } = await supabase
+    .from("users")
+    .select("*")
+    .eq("role", "Associate");
 
-    if (associatesError) {
-        throw createError({
-            statusCode : 500,
-            statusMessage : associatesError.message
-        })
-    }
+  if (associatesError) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: associatesError.message,
+    });
+  }
 
+  const { data: managers, error: managersError } = await supabase
+    .from("users")
+    .select("*")
+    .eq("role", "Manager");
 
-    const { data: managers, error: managersError } = await supabase
-        .from("users")
-        .select("*")
-        .eq("role","Manager")
+  if (managersError) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: managersError.message,
+    });
+  }
 
-    if (managersError) {
-        throw createError({
-            statusCode : 500,
-            statusMessage : managersError.message
-        })
-    }
+  const { data: admins, error: adminsError } = await supabase
+    .from("users")
+    .select("*")
+    .eq("role", "Admin");
 
-    const { data: admins, error: adminsError } = await supabase
-        .from("users")
-        .select("*")
-        .eq("role", "Admin");
+  if (adminsError) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: adminsError.message,
+    });
+  }
 
-    if (adminsError) {
-        throw createError({
-            statusCode : 500,
-            statusMessage : adminsError.message
-        })
-    }
+  const { data: users, error: usersError } = await supabase
+    .from("users")
+    .select("*")
+    .is("role", null);
 
-    const { data: users, error: usersError } = await supabase
-        .from("users")
-        .select("*")
-        .is("role",null)
+  if (usersError) {
+    console.log("invalid 4");
+    throw createError({
+      statusCode: 500,
+      statusMessage: usersError.message,
+    });
+  }
 
-    if (usersError) {
-        console.log("invalid 4")
-        throw createError({
-            statusCode : 500,
-            statusMessage : usersError.message
-        })
-    }
-
-    return { req_data: [admins,managers,associates,users]}
-})
+  return { req_data: [admins, managers, associates, users] };
+});
